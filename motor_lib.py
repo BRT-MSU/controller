2a90251925f4b53fc3a0b30bad4204c44d7b7aef
Alatec <johnsonkids.andrew@gmail.com>
2019-03-30 02:52:37 -0600

    edited motor_lib to use roboclaw3

--- motor_lib.py
+++ motor_lib.py
@@ -2,7 +2,7 @@ import re
 import enum
 import time
 from threading import Thread
-from roboclaw import Roboclaw
+from roboclaw3 import Roboclaw
 
 DEFAULT_TIME_TO_DELAY_MOTOR = 0.02  # 20 milliseconds
 
